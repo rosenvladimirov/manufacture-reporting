b173859e34a279bbd2dfaa9c986cd9bf20eda257
Rosen Vladimirov <vladimirov.rosen@gmail.cm>
2022-03-31 08:19:53 +0300

    some fixes in many modules and new added

---
+++ mrp_bom_structure_note/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 Rosen Vladimirov, BioPrint Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Mrp Bom Structure Note',
+    'summary': """
+        Add description in bom line row""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Rosen Vladimirov, BioPrint Ltd.,Odoo Community Association (OCA)',
+    'website': 'https://github.com/rosenvladimirov/manufacture-reporting',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+        'views/mrp_bom_view.xml',
+        'views/report_project_bom_views.xml',
+    ],
+    'demo': [
+    ],
+}
---
+++ mrp_bom_structure_note/models/mrp_bom.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class MrpBomLine(models.Model):
+    _inherit = 'mrp.bom.line'
+
+    description = fields.Char('Description', translate=True)
